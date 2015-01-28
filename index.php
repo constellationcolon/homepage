a6bc9195f25bc60171606759183e05c73841a523
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-27 23:30:43 -0500

    almost complete version of the gui for the event section.

--- index.php
+++ index.php
@@ -9,43 +9,96 @@
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="navbar navbar-default" role="navigation">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">CCSPS</a>
+    <div class="container-fluid" id="container">
+        <div class="row" id="billboard-wrap">
+            <div id="billboard-overlay">
+                <nav class="navbar navbar-default" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">CCSPS</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Events</a>
+                            </li>
+                            <li><a href="#">Resources</a>
+                            </li>
+                            <li><a href="#">Gallery</a>
+                            </li>
+                            <li><a href="#">About</a>
+                            </li>
+                            <li><a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="row" id="events">
+            <div class="col-xs-12" id="events-title">
+                <h1>EVENTS</h1>
+            </div>
+            <div class="event-wrap col-xs-12 col-sm-3">
+                <div class="event-date">
+                    <div class="event-date-month">FEB</div>
+                    <div class="event-date-day">12</div>
                 </div>
-                <div class="collapse navbar-collapse" id="collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Events</a>
-                        </li>
-                        <li><a href="#">Resources</a>
-                        </li>
-                        <li><a href="#">Gallery</a>
-                        </li>
-                        <li><a href="#">About</a>
-                        </li>
-                        <li><a href="#">Contact</a>
-                        </li>
-                    </ul>
+                <div class="event-name">
+                    <h3>Catching Up With Github</h3>
                 </div>
-            </nav>
+                <div class="event-description">
+                    <p id="blue">Aliquam sit amet accumsan nisi, ut facilisis neque. Vivamus in rhoncus elit. Vestibulum ornare dignissim semper. Proin tempus enim Vestibulum leo risus,ae,quis posuere est. uere est. Vestibulum leo risus, fringilla sit amet mauris vitae, Vestibulum leo risus, fringilla sit amet mauris vitae, viverra vehicula nulla. Aliquam hendrerit odio ex, id ultricies metus ornare non. Sed placerat condiment fringilla sit amet mauris vitae,quis posuere est. uere est. Vestibulum leo risus, fringilla sit amet mauris vitae, Vestibulum leo risus, fringilla sit amet mauris vitae, vivet quam blandit, vel faucibus dolor iaculis. Sed sit amet semper.</p>
+                </div>
+                <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
+            </div>
+            <div class="event-wrap col-xs-12 col-sm-3">
+                <div class="event-date">
+                    <div class="event-date-month">FEB</div>
+                    <div class="event-date-day">5</div>
+                </div>
+                <div class="event-name">
+                    <h3>Startup Career Fair</h3>
+                </div>
+                <div class="event-description">
+                    <p>Sed uere est. Vestibulum leo risus, fringilla sit amet mauris vitae,quis posuereae,quis posuere est. uere est. Vestibulum leo risus, fringilla laoreet.</p>
+                </div>
+                <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
+            </div>
+            <div class="event-wrap hidden-xs col-xs-12 col-sm-3">
+                <div class="event-date">
+                    <div class="event-date-month">JAN</div>
+                    <div class="event-date-day">28</div>
+                </div>
+                <div class="event-name">
+                    <h3>Blackhat Hacking</h3>
+                </div>
+                <div class="event-description">
+                    <p>Vivamus eros ex, commodo vitae turpis nec, eleifend iaculis augue. Fusce malesuada ipsum eget torae,quis posuere est. uere est. Vestibulum leo risus, fringilla sit amet mauris vitae, Vestibulum leo risus, fringilla sit amet mauris vitae, viverra vehicula nulla. Aliquam hendrerit odio ex, id ultricies metus ornare non. Sed placerat condimenttor condimentum tincidunt. Ut vel ligula et lectus gravida pulvinar sit amet sit amet leo.Vestibulum leo risus, fringilla sit amet mauris vitae,quis posuere est. uere est. Vestibulum leo risus, fringilla sit amet mauris vitae, Vestibulum leo risus, fringilla sit amet mauris vitae, viv Integer.</p>
+                </div>
+                <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
+            </div>
+        </div>
+        <div class="row" id="about-wrap">
+            <div id="about-overlay">
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+            </div>
         </div>
         <div class="row">
             <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
@@ -61,40 +114,6 @@
                 <div class="row">
                     <div class="hidden-xs col-sm-6">
                         <h2>About Us</h2>
-                    </div>
-                    <div class="pull-right-xs pull-right-sm pull-right-md pull-right-lg">
-                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                            <div class="row member-block">
-                                <div class="member-frame pale_blue">
-                                    <div id="costa" class="member-glass"></div>
-                                </div>
-                                <h5>[Full Name]</h5>
-                                <h6>[Position]</h6>
-                            </div>
-                            <div class="row member-block">
-                                <div class="member-frame pale_blue">
-                                    <div id="costa" class="member-glass"></div>
-                                </div>
-                                <h5>[Full Name]</h5>
-                                <h6>[Position]</h6>
-                            </div>
-                        </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                            <div class="row member-block">
-                                <div class="member-frame pale_blue">
-                                    <div id="costa" class="member-glass"></div>
-                                </div>
-                                <h5>[Full Name]</h5>
-                                <h6>[Position]</h6>
-                            </div>
-                            <div class="row member-block">
-                                <div class="member-frame pale_blue">
-                                    <div id="costa" class="member-glass"></div>
-                                </div>
-                                <h5>[Full Name]</h5>
-                                <h6>[Position]</h6>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="row">
@@ -130,10 +149,18 @@
         </div>
         <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
     </div>
-
     <!-- javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/clamp.min.js"></script>
+    <script type="text/javascript">
+        var elems = $('.event-description > p')
+        for (i = 0; i < elems.length; i++) {
+            $clamp(elems[i], {
+                clamp: 'auto'
+            });
+        }
+    </script>
 </body>
 
 </html>

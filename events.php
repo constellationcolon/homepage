d3a6ef3a2594a9a6b509490a7670c3bd9d6f9a88
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-01-29 02:38:01 -0500

    implements sticky header as well as correct coloring and transitioning for it.

--- events.php
+++ events.php
@@ -9,6 +9,7 @@
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link href="css/members.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -17,65 +18,41 @@
 </head>
 
 <body>
-    <div class="container-fluid" id="container">
-        <div class="row " id="billboard-wrap">
-            <div id="billboard-overlay">
-                <nav class="navbar navbar-default" role="navigation">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">CCSPS</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#events-wrap">EVENTS</a>
-                            </li>
-                            <li><a href="#about-wrap">ABOUT US</a>
-                            </li>
-                            <li><a href="#">RESOURCES</a>
-                            </li>
-                            <li><a href="#">GALLERY</a>
-                            </li>
-                            <li><a href="#contact-wrap">CONTACT</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">CCSPS</a>
+            </div>
+            <div class="collapse navbar-collapse" id="collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#events-wrap">EVENTS</a>
+                    </li>
+                    <li><a href="#about-wrap">ABOUT US</a>
+                    </li>
+                    <li><a href="#">RESOURCES</a>
+                    </li>
+                    <li><a href="#">GALLERY</a>
+                    </li>
+                    <li><a href="#contact-wrap">CONTACT</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+    <div class="container-fluid" id="container">
         <div class="row" id="members-wrap">
-            
+
         </div>
     </div>
     <!-- javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/clamp.min.js"></script>
-    <script type="text/javascript">
-        var elems = $('.event-description > p')
-        for (i = 0; i < elems.length; i++) {
-            $clamp(elems[i], {
-                clamp: 'auto'
-            });
-        }
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    </script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php
-    $events=array();
-    $cmd = "./sort.py events -d";
-    exec(escapeshellcmd($cmd), $output, $status);
-    if ($status) {
-        echo "exec command failed";
-    } else {
-        foreach($output as $line) {
-            array_push($events, json_decode($line));
-        }
-    }
-?>
+<?php $events=array(); $cmd="./sort.py events -d" ; exec(escapeshellcmd($cmd), $output, $status); if ($status) { echo "exec command failed"; } else { foreach($output as $line) { array_push($events, json_decode($line)); } } ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,35 +18,37 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">CCSPS</a>
+            </div>
+            <div class="collapse navbar-collapse" id="collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#events-wrap">EVENTS</a>
+                    </li>
+                    <li><a href="#about-wrap">ABOUT US</a>
+                    </li>
+                    <li><a href="#">RESOURCES</a>
+                    </li>
+                    <li><a href="#">GALLERY</a>
+                    </li>
+                    <li><a href="#contact-wrap">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container-fluid" id="container">
         <div class="row " id="billboard-wrap">
             <div id="billboard-overlay">
-                <nav class="navbar navbar-default" role="navigation">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">CCSPS</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#events-wrap">EVENTS</a>
-                            </li>
-                            <li><a href="#about-wrap">ABOUT US</a>
-                            </li>
-                            <li><a href="#">RESOURCES</a>
-                            </li>
-                            <li><a href="#">GALLERY</a>
-                            </li>
-                            <li><a href="#contact-wrap">CONTACT</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+
             </div>
         </div>
         <div class="row" id="events-wrap">
@@ -68,42 +59,51 @@
                 <div class="event-wrap col-xs-12 col-sm-4">
                     <div class="event-date">
                         <?php $date = date_create($events[2]->start_datetime) ?>
-                        <div class="event-date-month"><?php echo strtoupper($date->format('M')) ?></div>
-                        <div class="event-date-day"><?php echo $date->format('j') ?></div>
+                        <div class="event-date-month">
+                            <?php echo strtoupper($date->format('M')) ?></div>
+                        <div class="event-date-day">
+                            <?php echo $date->format('j') ?></div>
                     </div>
                     <div class="event-name">
                         <h3><?php echo $events[2]->name ?></h3>
                     </div>
                     <div class="event-description">
-                        <p><?php echo $events[2]->description ?></p>
+                        <p>
+                            <?php echo $events[2]->description ?></p>
                     </div>
                     <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
                 </div>
                 <div class="event-wrap col-xs-12 col-sm-4">
                     <div class="event-date">
                         <?php $date = date_create($events[1]->start_datetime) ?>
-                        <div class="event-date-month"><?php echo strtoupper($date->format('M')) ?></div>
-                        <div class="event-date-day"><?php echo $date->format('j') ?></div>
+                        <div class="event-date-month">
+                            <?php echo strtoupper($date->format('M')) ?></div>
+                        <div class="event-date-day">
+                            <?php echo $date->format('j') ?></div>
                     </div>
                     <div class="event-name">
                         <h3><?php echo $events[1]->name ?></h3>
                     </div>
                     <div class="event-description">
-                        <p><?php echo $events[1]->description ?></p>
+                        <p>
+                            <?php echo $events[1]->description ?></p>
                     </div>
                     <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
                 </div>
                 <div class="event-wrap hidden-xs col-xs-12 col-sm-4">
                     <div class="event-date">
                         <?php $date = date_create($events[0]->start_datetime) ?>
-                        <div class="event-date-month"><?php echo strtoupper($date->format('M')) ?></div>
-                        <div class="event-date-day"><?php echo $date->format('j') ?></div>
+                        <div class="event-date-month">
+                            <?php echo strtoupper($date->format('M')) ?></div>
+                        <div class="event-date-day">
+                            <?php echo $date->format('j') ?></div>
                     </div>
                     <div class="event-name">
                         <h3><?php echo $events[0]->name ?></h3>
                     </div>
                     <div class="event-description">
-                        <p><?php echo $events[0]->description ?></p>
+                        <p>
+                            <?php echo $events[0]->description ?></p>
                     </div>
                     <button type="button" class="event-btn btn btn-default col-xs-12 col-sm-12 col-md-12 col-lg-12">More</button>
                 </div>
@@ -174,6 +174,19 @@
                     }, 1000);
                     return false;
                 }
+            }
+        });
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 10) {
+                $('.navbar').css('background-color', 'rgba(68, 40, 26, 0.6)');
+                $('.navbar').css('-webkit-box-shadow', '0 1px 1px 0 rgba(0, 0, 0, 0.6)');
+                $('.navbar').css('box-shadow', '0 1px 1px 0 rgba(0, 0, 0, 0.6)');
+                $('.navbar-default .navbar-nav>li>a, .navbar-default .navbar-brand').css('color', '#D7D3D0');
+            } else {
+                $('.navbar').css('background-color', 'transparent');
+                $('.navbar').css('-webkit-box-shadow', 'none');
+                $('.navbar').css('box-shadow', 'none');
+                $('.navbar-default .navbar-nav>li>a, .navbar-default .navbar-brand').css('color', '#44281A');
             }
         });
     </script>

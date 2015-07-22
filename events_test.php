73dabd90644e0f6151d5c9ae38562f10effabeab
Matthew Haigh <mlh2196@columbia.edu>
2015-07-22 09:39:42 -0400

    finished mockup for all events

---
+++ events_test.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Columbia University Association for Computing Machinery</title>
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/events_test.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row events-all-wrapper col-md-12">
+            <div class="row events-search col-md-6">
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control search-text" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+            <div class="row events-list col-md-6">
+                <ul>
+                    <li>
+                        Event 1
+                    </li>
+                    <li>
+                        Event 2
+                    </li>
+                    <li>
+                        Event 3
+                    </li>
+                    <li>
+                        Event 4
+                    </li>
+                    <li>
+                        Event 5
+                    </li>
+                    <li>
+                        Event 6
+                    </li>
+                    <li>
+                        Event 7
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
